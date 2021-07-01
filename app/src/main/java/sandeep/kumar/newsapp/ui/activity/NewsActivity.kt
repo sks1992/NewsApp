40c010062ca43bc292b9289b0be274fe082488de
sks1992 <63912583+sks1992@users.noreply.github.com>
2021-07-01 18:43:18 +0530

    setupNavigation

--- app/src/main/java/sandeep/kumar/newsapp/ui/activity/NewsActivity.kt
+++ app/src/main/java/sandeep/kumar/newsapp/ui/activity/NewsActivity.kt
@@ -1,7 +1,8 @@
-package sandeep.kumar.newsapp
+package sandeep.kumar.newsapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sandeep.kumar.newsapp.R
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/sandeep/kumar/newsapp/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,21 @@
+package sandeep.kumar.newsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import sandeep.kumar.newsapp.R
+
+
+class SavedNewsFragment : Fragment() {
+   
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_breaking_news, container, false)
+    }
+}
